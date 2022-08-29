eb7243050c4c5f7537ecb6eb804cb27c83bf37e7
Mateusz Filipiak <mateusz.filipiak01@gmail.com>
2022-08-29 13:34:56 +0200

    Create a game mechanic

--- src/main.js
+++ src/main.js
@@ -54,37 +54,103 @@ const cardsColor = [
 const cards = document.querySelectorAll(".card");
 const tipBtns = document.querySelectorAll(".tip");
 const time = document.querySelector(".time");
+const trialsSelector = document.querySelector(".trials");
+const soundBtn = document.querySelector(".soundBtn");
+const randomArray = getRandom50NumbersArray();
+let previousCard;
+let previousCardColor;
+let nextCard;
+let nextCardColor;
+let soundPermission = true;
 let seconds = 0;
+let secondsDisplay = 0;
+let minutes = 0;
+let hours = 0;
 let trials = 0;
+let trialsNumber = 0;
 
 setInterval(timer, 1000);
+showAllCardsColors(5000);
 
 cards.forEach((card, index) => {
   card.addEventListener("click", () => play(card, index));
 });
 
+soundBtn.addEventListener("click", () => {
+  const soundBtnIcon = document.querySelector(".soundBtn i");
+  if (soundPermission) {
+    soundPermission = false;
+    soundBtnIcon.classList.remove("icon-volume-up");
+    soundBtnIcon.classList.add("icon-volume-off");
+  } else {
+    soundPermission = true;
+    soundBtnIcon.classList.remove("icon-volume-off");
+    soundBtnIcon.classList.add("icon-volume-up");
+  }
+});
+
 tipBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    showAllCardsColors();
+    showAllCardsColors(3000);
     e.target.classList.add("disable");
   });
 });
 
 function play(card, i) {
-  if (trials < 2) {
-    showCardColor(card, i);
-    trials++;
-  } else {
-    return;
-    trials = 0;
+  switch (trials) {
+    case 0:
+      showCardColor(card, i);
+      previousCard = card;
+      previousCardColor = card.style.backgroundColor;
+      trials++;
+      break;
+    case 1:
+      showCardColor(card, i);
+      nextCard = card;
+      if (nextCard === previousCard) return;
+      nextCardColor = card.style.backgroundColor;
+      console.log(`1 color: ${previousCardColor}\n2 color: ${nextCardColor}`);
+      console.log(
+        `Czy to te same kolory?: ${previousCardColor === nextCardColor}`
+      );
+      if (previousCardColor === nextCardColor) {
+        hideOrDeleteCard("delete");
+      } else {
+        hideOrDeleteCard("hide");
+      }
+      trials = 0;
+      trialsNumber++;
+      trialsSelector.textContent = trialsNumber;
+      break;
   }
-  //trials > 2 ? (trials = 0) : trials++;
+
+  function hideOrDeleteCard(operation) {
+    cards.forEach((card) => {
+      card.style.pointerEvents = "none";
+    });
+    setTimeout(() => {
+      previousCard.style.backgroundColor = "";
+      nextCard.style.backgroundColor = "";
+      previousCard.classList.add(operation);
+      nextCard.classList.add(operation);
+      cards.forEach((card) => {
+        card.style.pointerEvents = "";
+      });
+    }, 1000);
+  }
+
+  function showCardColor(card, index) {
+    card.style.backgroundColor = cardsColor[randomArray[index]];
+    card.classList.remove("hide");
+  }
 }
 
-function showAllCardsColors() {
+function showAllCardsColors(time) {
   cards.forEach((card, index) => {
-    card.style.backgroundColor = cardsColor[index];
-    card.classList.remove("hide");
+    if (!card.classList.contains("delete")) {
+      card.style.backgroundColor = cardsColor[randomArray[index]];
+      card.classList.remove("hide");
+    }
   });
 
   setTimeout(() => {
@@ -92,21 +158,41 @@ function showAllCardsColors() {
       card.classList.add("hide");
       card.style.backgroundColor = "";
     });
-  }, 2000);
-}
-
-function showCardColor(card, index) {
-  card.style.backgroundColor = cardsColor[index];
-  card.classList.remove("hide");
+  }, time);
 }
 
 function timer() {
-  const minutes = Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
-
-  time.textContent = `Time: ${hours < 10 ? "0" + hours : hours}:${
+  if (secondsDisplay >= 59) {
+    secondsDisplay = 0;
+    minutes++;
+  } else {
+    secondsDisplay++;
+  }
+  if (minutes >= 59) {
+    minutes = 0;
+    hours++;
+  }
+  time.textContent = `${hours < 10 ? "0" + hours : hours}:${
     minutes < 10 ? "0" + minutes : minutes
-  }:${seconds < 10 ? "0" + seconds : seconds}`;
+  }:${secondsDisplay < 10 ? "0" + secondsDisplay : secondsDisplay}`;
 
-  seconds === 60 ? (seconds = 0) : seconds++;
+  seconds++;
+}
+
+function getRandom50NumbersArray() {
+  const array = [];
+  while (array.length < 50) {
+    const randomNumber = Math.floor(Math.random() * 50);
+    let flag = true;
+    for (let j = 0; j < 50; j++) {
+      if (randomNumber === array[j]) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      array.push(randomNumber);
+    }
+  }
+  return array;
 }
